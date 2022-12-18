1c487098aee3dfba6dea2c0f0e87465a3a0ac5db
sagarniroula112 <45513738+sagarniroula112@users.noreply.github.com>
2022-12-18 17:54:48 +0545

    Simple Python Calculator is Complete!

---
+++ simplecalculatorusingpy.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+sum = num1 + num2
+
+diff = num1 - num2
+
+mult = num1 * num2
+
+div = num1 / num2
+
+print(sum)
+print(diff)
+print(mult)
+print(div)
