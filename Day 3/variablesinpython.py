50d8fc73290a628bed0e932c30c06b17bfdba4e5
sagarniroula112 <45513738+sagarniroula112@users.noreply.github.com>
2022-12-18 17:06:43 +0545

    I have committed one file

---
+++ Day 3/variablesinpython.py
@@ -0,0 +1,10 @@
+import pandas
+print("Hi")
+print("Hello Nepal!, 9")
+numlist = [1, 2, 3, "Sagar"]
+print(numlist[0], "of", numlist[3])
+dict = {"name": "Sagar", "age": 20, "canVote": True}
+tuple = ("sagar", 1)
+# tuple[0] = "Harry"  // Tuple- Normal round brackets  list- Square brackets  Dictionary- Curly brackets
+
+print(tuple[0])
